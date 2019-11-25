6378cc25319bd433706f53c58237527359f009a4
sndz <506027572@qq.com>
2019-11-25 16:14:22 +0800

    add function

--- product.py
+++ product.py
@@ -1,40 +1,49 @@
 import os
 #读取档案
-products = []
-products_1 = []
-if os.path.isfile('products.csv'):
-    print('yeah')
-    with open('products.csv', 'r') as f:
-        for line in f:
-            if '商品,价格' in line:
-                continue
-            s = line.strip().split(',')
-            products.append(s)
-            name, price = line.strip().split(',')
-            products_1.append([name, price])
-            print(s)
-            print(name)
-            print(products)
-            print(products_1)
-else:
-    print ('no')
+def readfile(file_name):
+    products = []
+    products_1 = []
+    if os.path.isfile(file_name):
+        print('yeah')
+        with open(file_name, 'r') as f:
+            for line in f:
+                if '商品,价格' in line:
+                    continue
+                s = line.strip().split(',')
+                products.append(s)
+                name, price = line.strip().split(',')
+                products_1.append([name, price])
+                print(s)
+                print(name)
+                print(products)
+                print(products_1)
+    else:
+        print('no')
+    return products_1
+
 
 #让使用者输入，并存储到文件中
-while True:
-    name = input("please input name")
-    if name == 'q':
-        break
-    price = input("please input the price")
-    # product =[]
-    # product.append(name)
-    # product.append(price)
-    #product = [name,price]
-    products.append([name,price])
-print(products)
+def user_input(products):
+    while True:
+        name = input("please input name")
+        if name == 'q':
+            break
+        price = input("please input the price")
+        # product =[]
+        # product.append(name)
+        # product.append(price)
+        # product = [name,price]
+        products.append([name, price])
+    print(products)
+    return products
 
 #写入文件
-with open('products.csv', 'w') as f:
-    f.write('商品,价格\n')
-    for p in products:
-        f.write(p[0] + ',' + p[1]+ '\n')
+def write_file(file_name, products):
+    with open(file_name, 'w') as f:
+        f.write('商品,价格\n')
+        for p in products:
+            f.write(p[0] + ',' + p[1] + '\n')
+
+readfile('products.csv')
+
 
