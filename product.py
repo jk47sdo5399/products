946b6c3a14e9e899ab6890586b67070424a2a533
sndz <506027572@qq.com>
2019-11-25 16:45:15 +0800

    add function

--- product.py
+++ product.py
@@ -1,26 +1,22 @@
 import os
+
 #读取档案
-def readfile(file_name):
+def read_file(file_name):
     products = []
     products_1 = []
-    if os.path.isfile(file_name):
-        print('yeah')
-        with open(file_name, 'r') as f:
-            for line in f:
-                if '商品,价格' in line:
-                    continue
-                s = line.strip().split(',')
-                products.append(s)
-                name, price = line.strip().split(',')
-                products_1.append([name, price])
-                print(s)
-                print(name)
-                print(products)
-                print(products_1)
-    else:
-        print('no')
-    return products_1
-
+    with open(file_name, 'r') as f:
+        for line in f:
+            if '商品,价格' in line:
+                continue
+            s = line.strip().split(',')
+            products.append(s)
+            name, price = line.strip().split(',')
+            products_1.append([name, price])
+            print(s)
+            print(name)
+            print(products)
+            print(products_1)
+    return products
 
 #让使用者输入，并存储到文件中
 def user_input(products):
@@ -44,6 +40,15 @@ def write_file(file_name, products):
         for p in products:
             f.write(p[0] + ',' + p[1] + '\n')
 
-readfile('products.csv')
+def main():
+    file_name = 'products.csv'
+    if os.path.isfile(file_name):
+        print('yeah')
+        products = read_file(file_name)
+    else:
+        print('no')
+    products = user_input(products)
+    write_file('products.csv',products)
 
-
+if __name__ == '__main__':
+    main()
